c44a62c2f83da24eb252cd4cf283cb91b6c34ae7
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-05-24 06:39:57 +0300

    Initial Commit

---
+++ Lifar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lifar
+//
+//  Created by Oleksandr Smakhtin on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
